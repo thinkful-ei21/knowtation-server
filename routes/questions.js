69bc2a9bde9a6b3e324648ee24dc9aeccb2f11ce
chagoy <angelochagoy@gmail.com>
2018-08-09 08:58:10 -0700

    new questions file

--- routes/questions.js
+++ routes/questions.js
@@ -1,28 +1,28 @@
 'use strict';
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const router = express.Router();
-const jsonParser = bodyParser.json();
 const { Question } = require('../models/question');
 const { User } = require('../models/user');
-const { Result } = require('../models/result');
 
 /** import auth stuff **/
 const passport = require('passport');
 router.use(passport.authenticate('jwt', { sessions: false, failWithError: true}));
 
 /** GET endpoint - should return only 1 question **/
-router.get('/', jsonParser, (req, res, next) => {
+router.get('/', (req, res, next) => {
     let id = req.user.id;
-    let user = User.findById(id).populate('questions.question')
+    User.findById(id)
         .then(user => user.questions[user.head])
-        .then(data => res.json(data))
-        .catch(err => console.err(err));
+        .then(data => {
+            const { question, numCorrect, numAttempts } = data;
+            return res.json({ question, numCorrect, numAttempts });
+        })
+        .catch(err => console.log(err));
 });
 
 /** POST endpoint - to submit a question **/
-router.post('/submit', jsonParser, (req, res, next) => {
+router.post('/submit', (req, res, next) => {
     let { question, answer, hint, title, explanation } = req.body;
     return Question.create({question: encodeURI(question), answer, hint, title, explanation})
         .then(question => {
@@ -37,43 +37,60 @@ router.post('/submit', jsonParser, (req, res, next) => {
 });
 
 /** POST answer/result to an endpoint **/
-router.post('/answer', jsonParser, (req, res, next) => {
-    let {answer, question} = req.body;
+router.post('/answer', (req, res, next) => {
+    let {answer} = req.body;
     let {id} = req.user;
-    let result;
+    let response;
 
-    return User.findById(id)
-            .then(user => )
-
-    return Question.findById(question)
-                    .then(question => {
-                        result = question.answer === answer ? true : false;
-                        Result.create({question, user: id, result});
-                        return question;
-                    }).then(question => {
-                        if (result) {
-                            return User.findById(id)
-                                .then(user => {
-                                    user.head = question.next;
-                                    if (user.questions[user.questions.length - 1].next === null) {
-                                        user.questions[user.questions.length - 1].next = question;
-                                        question.next = null;
-                                    }
-                                    user.save();
-                                })
-                        }
-
-        })
-        .then(() => res.json(result))
-                    .catch(err => console.error(err))
-})
-
-/*** HELPER METHODS **/
-const removeFirstQuestion = id => {
-    return User.findById({id})
+    User.findById(id)
         .then(user => {
-            return user.
+            const currentQuestion = user.questions[user.head];
+            const currentIndex = user.head;
+
+            if (currentQuestion.answer === answer) {
+                response = true;
+                currentQuestion.numCorrect++;
+                currentQuestion.numAttempts++;
+                currentQuestion.mValue *= 2;
+            } else {
+                response = false;
+                currentQuestion.numAttempts++;
+                currentQuestion.mValue = 1;
+            }
+
+            user.head = currentQuestion.next;
+
+            let insertAfter = currentQuestion;
+            let temp = currentQuestion;
+
+            for (let i = 0; i < currentQuestion.mValue; i++) {
+                let index = temp.next;
+                if (currentQuestion.mValue > user.questions.length) {
+                    currentQuestion.mValue = user.questions.length - 1;
+                    index = user.questions.length - 1;
+                }
+                insertAfter = user.questions[index];
+                temp = user.questions[temp.next];
+            }
+
+            if (insertAfter === null) {
+                currentQuestion.next = null;
+            } else {
+                currentQuestion.next = insertAfter.next;
+            }
+
+            insertAfter.next = currentIndex;
+
+            user.save();
+
+            return currentQuestion;
         })
-}
+        .then(question => {
+            const { answer, numCorrect, numAttempts } = question;
+            return res.json({ response, answer, numCorrect, numAttempts });
+        })
+        .catch(err => console.err(err));
+
+});
 
 module.exports = { router };
